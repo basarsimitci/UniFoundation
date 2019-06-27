97ae1b67f575828504662f066ee1e3b29337bab9
Basar Simitci <basar.simitci@joyful.works>
2019-06-27 20:07:41 +0100

    Fixed hex colour string to colour conversions.

--- Extensions/Colour/Color32Extensions.cs
+++ Extensions/Colour/Color32Extensions.cs
@@ -1,33 +1,19 @@
-using System.Globalization;
+using System;
 using UnityEngine;
 
 namespace UniFoundation.Extensions.Colour
 {
     public static class Color32Extensions
     {
-        public static void FromHexString(this Color32 value, string hexString)
+        public static string ToHexString(this Color value)
         {
-            if ((hexString.StartsWith("#") == false) ||
-                (((hexString.Length == 4) || (hexString.Length == 7)) == false))
-            {
-                return;
-            }
-
-            string redString = hexString.Length == 4
-                ? hexString.Substring(1, 1)
-                : hexString.Substring(1, 2);
-            string greenString = hexString.Length == 4
-                ? hexString.Substring(2, 1)
-                : hexString.Substring(3, 2);
-            string blueString = hexString.Length == 4
-                ? hexString.Substring(3, 1)
-                : hexString.Substring(5, 2);
-
-            byte.TryParse(redString, NumberStyles.HexNumber, null, out value.r);
-            byte.TryParse(greenString, NumberStyles.HexNumber, null, out value.g);
-            byte.TryParse(blueString, NumberStyles.HexNumber, null, out value.b);
+            string hexString = "#";
+            hexString += Convert.ToByte(value.r).ToString("x2");
+            hexString += Convert.ToByte(value.g).ToString("x2");
+            hexString += Convert.ToByte(value.b).ToString("x2");
+            return hexString;
         }
-
+        
         public static string ToHexString(this Color32 value)
         {
             string hexString = "#";
--- Extensions/Types/StringExtensions.cs
+++ Extensions/Types/StringExtensions.cs
@@ -1,3 +1,6 @@
+using System.Globalization;
+using UnityEngine;
+
 namespace UniFoundation.Extensions.Types
 {
     public static class StringExtensions
@@ -23,6 +26,33 @@ namespace UniFoundation.Extensions.Types
             }
 
             return 0;
+        }
+        
+        public static Color32 ToColor32(this string value)
+        {
+            Color32 colour = Color.clear;
+
+            if ((value.StartsWith("#") == false) ||
+                (((value.Length == 4) || (value.Length == 7)) == false))
+            {
+                return colour;
+            }
+
+            string redString = value.Length == 4
+                ? value.Substring(1, 1)
+                : value.Substring(1, 2);
+            string greenString = value.Length == 4
+                ? value.Substring(2, 1)
+                : value.Substring(3, 2);
+            string blueString = value.Length == 4
+                ? value.Substring(3, 1)
+                : value.Substring(5, 2);
+
+            byte.TryParse(redString, NumberStyles.HexNumber, null, out colour.r);
+            byte.TryParse(greenString, NumberStyles.HexNumber, null, out colour.g);
+            byte.TryParse(blueString, NumberStyles.HexNumber, null, out colour.b);
+            
+            return colour;
         }
     }
 }
