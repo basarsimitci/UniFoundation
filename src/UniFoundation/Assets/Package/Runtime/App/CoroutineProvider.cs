431fcaeee4acc7b0d9f4dc21cda838c7633059e4
Basar Simitci <basar@solarflarestudio.co.uk>
2021-11-03 12:00:11 +0000

    Removed methofs from CoroutineProvider since MonoBehaviour's coroutine methods are already public.

--- src/UniFoundation/Assets/Package/Runtime/App/CoroutineProvider.cs
+++ src/UniFoundation/Assets/Package/Runtime/App/CoroutineProvider.cs
@@ -1,18 +1,8 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace JoyfulWorks.UniFoundation.App
 {
     public class CoroutineProvider : MonoBehaviour
     {
-        public Coroutine Start(IEnumerator coroutineEnumerator)
-        {
-            return StartCoroutine(coroutineEnumerator);
-        }
-
-        public void Stop(Coroutine coroutine)
-        {
-            StopCoroutine(coroutine);
-        }
     }
 }
