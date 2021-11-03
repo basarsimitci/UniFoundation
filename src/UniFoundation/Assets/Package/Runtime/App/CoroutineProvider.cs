fa0956fc32b231a17e3089d2d7412001742a1f78
Basar Simitci <basar@solarflarestudio.co.uk>
2021-11-03 11:53:38 +0000

    Added CoroutineProvider

---
+++ src/UniFoundation/Assets/Package/Runtime/App/CoroutineProvider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace JoyfulWorks.UniFoundation.App
+{
+    public class CoroutineProvider : MonoBehaviour
+    {
+        public Coroutine Start(IEnumerator coroutineEnumerator)
+        {
+            return StartCoroutine(coroutineEnumerator);
+        }
+
+        public void Stop(Coroutine coroutine)
+        {
+            StopCoroutine(coroutine);
+        }
+    }
+}
